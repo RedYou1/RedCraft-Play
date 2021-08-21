67bda4e70ae5e7a582c52b22c8eadc23503d6a24
RedYou <63940364+RedYou1@users.noreply.github.com>
2021-08-20 22:52:47 -0400

    bug in game console

--- see console Game.bat
+++ see console Game.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p name=Wich name?: 
-java -Xms512M -Xmx1024M -jar Game.jar "%name%" "C:\Users\jcdem\OneDrive\Bureau\RedCraft third writing\Games\test"
+java -Xms512M -Xmx1024M -jar Game.jar "%name%" "Games\test"
 pause
